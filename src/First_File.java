d86555af270ebb95675bfb1da05f4d8e78d0f67d
Zaryab Hassan <zaryabh49@gmail.com>
2021-12-04 18:02:54 +0500

    My first commit

---
+++ src/First_File.java
@@ -0,0 +1,11 @@
+public class First_File {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Zaryab Hassan";
+		String rollno = "20i-2487";
+		System.out.println("Name" + name);
+		System.out.println("RollNo: " + rollno);
+	}
+
+}
